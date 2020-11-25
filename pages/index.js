193b93ec1b039ce4b6fd67fd34eaa96d7a3e87c4
erickotd <erickotd@gmail.com>
2020-11-25 16:25:50 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+        <Title>Prueba</Title>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
